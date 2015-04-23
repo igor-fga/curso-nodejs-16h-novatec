98d19e0b2e2f857fda2a39de51e50c63f1974ae3
William Bruno <rocha_bruno@hotmail.com>
2015-04-22 23:01:01 -0300

    Added error handling

--- app.js
+++ app.js
@@ -20,6 +20,34 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', require('./routes')(app));
+// app.use('/', require('./routes')(app));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+/* istanbul ignore else */
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500).json({ err: err.message });
+//   });
+// }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
